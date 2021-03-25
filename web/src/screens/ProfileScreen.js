64d2536f6f2f1fc3a4695d24591a58d3bd94e5b2
kiruthik_raaj <kiruthik@pinesphere.in>
2021-03-25 18:50:52 +0530

    error handling

--- web/src/screens/ProfileScreen.js
+++ web/src/screens/ProfileScreen.js
@@ -18,6 +18,7 @@ export default function ProfileScreen({
   const [type, setType] = useState("");
   const [list, setList] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [ignored, setIgnored] = useState(false);
 
   async function renderPatents() {
     if (fromAddress) {
@@ -98,10 +99,20 @@ export default function ProfileScreen({
             </div>
           </div>
         </div>
+      ) : ignored ? (
+        <div className="card d-flex align-items-center justify-content-between p-5">
+          <h3 id="default-card">Login to Metamask</h3>
+          <div class="example-column example-column-1">
+            <div class="card p-7">
+              Your Metamask is not connected or logged in.
+            </div>
+          </div>
+        </div>
       ) : (
         <AlertComponent
           error={error}
           setError={setError}
+          setIgnored={setIgnored}
           onCancel={(state) => setTransferModalVisible(state)}
         />
       )}
